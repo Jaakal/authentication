7291a7d5eda98df3373dda34836b3a3e3d4a4001
Yair Facio <yair.facio11@gmail.com>
2020-02-12 15:16:11 -0600

    Displaying posts and redirecting the user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :is_logged_in?, only: [:new, :create]
   
   def index
+    @posts = Post.all
   end
 
   def new
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_url
+      redirect_to posts_path
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in?
-    redirect_to root_url
+    redirect_to posts_path
   end
 end
